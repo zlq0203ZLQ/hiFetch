a7c937fe3c72fa7ab2f9afae6fbebe7c9a901a54
liquanZ <zlq_0203@163.com>
2018-10-08 15:36:12 +0800

    带又token的fetch封装

---
+++ ohFetch/request/request.js
@@ -0,0 +1,37 @@
+import { hashHistory } from 'react-router-dom'
+
+export default request = (method,url,body)=>{
+	method = method.toUpperCase();
+	if (method == 'GET'){
+		body = undefined
+	}else{
+		body = body && JSON.stringify(body);
+	}
+
+	return fetch(url,{
+		headers: new Headers({
+			'Accept': 'application/json',
+			'Content-Type': 'application',
+			'Access-Token': sessionStorage.getItem('access-token')
+		}),
+		method: method,
+		body： body
+	})
+		.then(res=>{
+			if(res.status == 401){
+				hashHistory.push('/login')
+				return Promise.reject('unauthorized')
+			}else{	
+				const token = res.headers.get('access-token')
+				if(token){
+					sessionStorage.setItem('access-token',token)
+				}
+				return res.json()
+			}
+		});
+
+		export const get = url => request('GET',url);
+		export const post = (url,body) => request('POST',url,body);
+		export const put = (url,body) => request('PUT',url,body);
+		export const del = (url,body)=> request('DELETE',url,body)
+}
